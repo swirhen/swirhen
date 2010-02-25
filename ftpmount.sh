e4969a5fbe8a8b260828c4caae61df015e496729
swirhen <swirhen@hmx-51seto.(none)>
2010-02-26 07:02:33 +0900

    あわわ

--- ftpmount.sh
+++
@@ -1,3 +0,0 @@
-curlftpfs -o user=swirhen:npQWBEDhhxy,uid=1000,gid=1000 swirhen.bashi.org /data/share/ftpfs/bashi
-#curlftpfs -o user=swirhen:shunirr,uid=1000,gid=1000,ftp_port=40040 shunidex.shunirr.org /data/share/ftpfs/shunirr
-#curlftpfs -o user=dankogai:kogaidan,uid=1000,gid=1000 ssig33.ddo.jp /data/share/ftpfs/ssig33

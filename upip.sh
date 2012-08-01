7fc0b6dedc2c0b124b1edca4f1a0fb32e3e79d91
swirhen <swirhen@gmail.com>
2012-08-01 13:30:07 +0900

    うーむ

--- upip.sh
+++ upip.sh
@@ -1,4 +1,8 @@
-wget http://swirhen.bashi.org/filemanager.cgi -O /tmp/tmp.txt
-grep member /tmp/tmp.txt > /tmp/myip.txt
-ncftpput -u swirhen -p npQWBEDhhxy swirhen.bashi.org /public_html /tmp/myip.txt
-rm /tmp/myip.txt /tmp/tmp.txt
+wget http://www.luft.co.jp/cgi/ipcheck.php -O /tmp/myip.html
+#wget http://swirhen.net/cgi-bin/ipcheck.cgi -O /tmp/myip.html
+#grep name=\"IP\" /tmp/tmp.html | cut -d" " -f4 | cut -d"\"" -f2 > /tmp/myip.txt
+cp /tmp/myip.html /home/swirhen/Dropbox/temp/
+#ncftpput -u swirhen -p irankae swirhen.net /httpdocs /tmp/myip.html
+#ncftpput -u swirhen -p irankae swirhen.net /httpdocs /tmp/myip.html
+#rm /tmp/myip.html /tmp/tmp.txt
+rm /tmp/myip.html

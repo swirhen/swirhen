67a6c1d61fd5af76e9a7dcdcaace6016a4fd5399
swirhen <swirhen@gmail.com>
2012-08-01 13:44:51 +0900

    せいり

---
+++ tiarrakw.sh
@@ -0,0 +1,4 @@
+ps -ef | grep tiarra.conf | grep -v grep | awk '{print $2}' | xargs kill -9
+sleep 10
+cd /home/swirhen
+nohup /home/swirhen/tiarra/tiarra --config=/home/swirhen/tiarra/tiarra.conf >/dev/null 2>&1 &

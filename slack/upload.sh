17f5d4556cf8baa3a6fa89fb4391f713bea556d9
swirhen <irankae1#>
2017-04-26 16:19:11 +0900

    slack bot

--- slack/upload.sh
+++ slack/upload.sh
@@ -5,4 +5,4 @@ FILENAME=$2
 TITLE=$3
 TOKEN=`cat ${SCRIPT_DIR}/token`
 
-curl -F channels=${CHANNEL} -F file=@${FILENAME} -F title=${TITLE} -F token=${TOKEN} https://slack.com/api/files.upload
+curl -F channels=${CHANNEL} -F file=@"${FILENAME}" -F title=${TITLE} -F token=${TOKEN} https://slack.com/api/files.upload

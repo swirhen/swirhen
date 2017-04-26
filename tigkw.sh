8b696e859f22f7f05ea8ba9a609046ff86a81e43
swirhen <swirhen@gmail.com>
2017-04-26 12:00:30 +0900

    なんかしゅうせい

--- tigkw.sh
+++ tigkw.sh
@@ -1,3 +1,4 @@
 kill `cat /home/swirhen/tig.pid`
 sleep 10
-nohup /home/swirhen/TIG/TwitterIrcGateway --BroadcastUpdate=true --port=16668 --bind-address=0.0.0.0 --encoding=utf-8 --disable-userlist=true --set-topic-onstatuschanged=true --broadcast-update-message-is-notice=true --enable-compression=false --disable-notice-at-first-time=true >/dev/null 2>&1 & echo "TIG beta restarted at "`date` > /home/swirhen/tig.restart
+cd /home/swirhen/TIG
+nohup /home/swirhen/TIG/TwitterIrcGateway --BroadcastUpdate=true --port=16668 --bind-address=0.0.0.0 --encoding=utf-8 --disable-userlist=true --set-topic-onstatuschanged=true --broadcast-update-message-is-notice=true --enable-compression=false --disable-notice-at-first-time=true --OAuthClientKey=Cjx5NIRxdYKuTbKZbZHfR1AVM --OAuthSecretKey=rYHeaHLwnaFOC3XG2qwcyy3yvHNSKvpJRjfHHcqDQqWB5ZZwVl >/dev/null 2>&1 & echo "TIG beta restarted at "`date` > /home/swirhen/tig.restart

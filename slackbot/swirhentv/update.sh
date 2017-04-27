8a2f42d1cc1bb3720b98f53ce41ae89de3d53e92
swirhen <irankae1#>
2017-04-27 10:59:36 +0900

    slack bot

---
+++ slackbot/swirhentv/update.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+upflg=$1
+channel=$2
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+
+if [ "$channel" = "" ]; then
+  channel="bot-sandbox"
+fi
+
+logfile="${script_dir}/temp/update_restart.log"
+rm -f ${logfile}
+
+if [ "${upflg}" = "1" -o "${upflg}" = "2" ]; then
+  echo "*** slackbot 自己更新 ***" | tee -a ${logfile}
+
+  echo "git pull" | tee -a ${logfile}
+  git pull origin master | tee -a ${logfile}
+
+  echo "chmod +x *.sh" | tee -a ${logfile}
+  chmod +x *.sh | tee -a ${logfile}
+fi
+
+if [ "${upflg}" != "2" ]; then
+  echo "*** slackbot 再起動 ***" | tee -a ${logfile}
+
+  echo "kill `cat ${script_dir}/slackbot.pid`" | tee -a ${logfile}
+  kill `cat ${script_dir}/slackbot.pid` | tee -a ${logfile}
+
+  echo "python run.py" | tee -a ${logfile}
+  python run.py | tee -a ${logfile} &
+fi
+
+if [ "${upflg}" = "1" ]; then
+  python post.py "${channel}" "$HOSTNAME slackbot 自己更新 & 再起動しました
+"'```'"`cat ${logfile}`"'```'
+elif [ "${upflg}" = "2" ]; then
+  python post.py "${channel}" "$HOSTNAME slackbot 自己更新しました
+"'```'"`cat ${logfile}`"'```'
+else
+  python post.py "${channel}" "$HOSTNAME slackbot 再起動しました"
+fi
---
+++ slackbot/swirhentv/upload.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# slackモジュール(snippet)
+# tokenが @ppm_dev_bot のものなので、プライベートグループへの発言は @ppm_dev_bot がchannnelにjoinしている必要あり
+# 第1引数：発言チャンネル
+# 第2引数：ファイル名
+# 第3引数：ファイルタイトル
+# 第4引数：ファイルタイプ(省略時plain text)
+# ファイルタイプ例: https://api.slack.com/types/file#file_types
+
+from slacker import Slacker
+import sys
+import slackbot_settings
+
+
+def usage():
+    print ('usage: ', args[0], '[post_channel] [file_name] [file_title] (filetype)')
+    sys.exit(1)
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) < 3:
+        usage()
+
+    channel = args[1]
+    filename = args[2]
+    filetitle = args[3]
+
+    if len(args) > 4:
+        filetype = args[4]
+    else:
+        filetype = 'text'
+
+    slack = Slacker(slackbot_settings.API_TOKEN)
+    slack.files.upload(
+        filename,
+        filename=filetitle,
+        filetype=filetype,
+        channels=channel,
+        )
+

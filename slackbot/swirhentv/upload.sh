968b0871cd655989cb06f3b9e548814ffb261862
swirhen <irankae1#>
2017-04-27 17:51:18 +0900

    slack bot

---
+++ slackbot/swirhentv/upload.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+channel=$1
+filename=$2
+filetitle=$3
+if [ "" != "$4" ]; then
+  filetype=$4
+else
+  filetype=text
+fi
+
+DATETIME="`date '+%Y%m%d'`"
+token=`cat ${script_dir}/token`
+
+echo "curl -F channels=${channel} -F file=@${filename} -F title=${filetitle} -F token=${token} -F filetype=${filetype} https://slack.com/api/files.upload -k"
+
+curl -F channels=${channel} -F file=@${filename} -F title=${filetitle} -F token=${token} https://slack.com/api/files.upload -k

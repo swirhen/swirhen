861fa84110b2cc4347c3f6261bfe9a4c9cef84fd
swirhen <irankae1#>
2017-04-27 13:48:17 +0900

    slack bot

--- checker/torrentsearch/torrentsearch.sh
+++ checker/torrentsearch/torrentsearch.sh
@@ -18,10 +18,8 @@ curl -L -X GET "${URI}&offset=3&page=rss" >> ${SCRIPT_DIR}/${LISTNAME}.crawl
 
 while read keyword
 do
-  echo "keyword: ${keyword}"
   src=`cat ${SCRIPT_DIR}/${LISTNAME}.crawl | grep "${keyword}"`
   if [ "${src}" != "" ]; then
-    echo "hit"
     flg=1
     /home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "d swirhen 【汎用種調査 ${DATE}】検索キーワード ${keyword} リスト名: ${LISTNAME} ヒットしました！"
     /home/swirhen/sh/slack/post.sh "swirhentv" "@here 【汎用種調査 ${DATE}】検索キーワード ${keyword} リスト名: ${LISTNAME} ヒットしました！"

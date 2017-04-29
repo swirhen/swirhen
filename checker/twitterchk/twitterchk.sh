801bdb3a6856fe260abee8a1656dde9c7749d51a
swirhen <irankae1#>
2017-04-30 02:56:06 +0900

    いろいろ修正

--- checker/twitterchk/twitterchk.sh
+++ checker/twitterchk/twitterchk.sh
@@ -12,39 +12,55 @@ SEARCH_WORD=$2
 SEARCH_WORD2=$3
 SEARCH_WORD3=$4
 
-HIT=`cat /data/share/log/${CHANNEL}/${DATE}.txt | sed -n -e '/^search: ここまで読んだ/,$p' | grep "${SEARCH_WORD}" -3`
-if [ "${SEARCH_WORD2}" != "" ]; then
-  HIT2=`cat /data/share/log/${CHANNEL}/${DATE}.txt | sed -n -e '/^search: ここまで読んだ/,$p' | grep "${SEARCH_WORD2}" -3`
-else
-  HIT2="HIT"
-fi
-if [ "${SEARCH_WORD3}" != "" ]; then
-  HIT3=`cat /data/share/log/${CHANNEL}/${DATE}.txt | sed -n -e '/^search: ここまで読んだ/,$p' | grep "${SEARCH_WORD3}" -3`
-else
-  HIT3="HIT"
-fi
+cnt=0
+D_T_C_N=()
+TEXT=()
+cat /data/share/log/${CHANNEL}/${DATE}.txt | sed -n -e '/^search: ここまで読んだ/,$p' | while read LOGDATE LOGTIME CHANNEL_AND_NICK TEXT
+do
+  if [ "${DATE}" = "search:" ]; then
+    continue
+  fi
+  if [ "${D_T_C_N[${cnt}]}" != "${LOGDATE} ${LOGTIME} ${CHANNEL_AND_NICK}" ]; then
+    (( cnt++ ))
+    D_T_C_N[${cnt}]="${LOGDATE} ${LOGTIME} ${CHANNEL_AND_NICK}"
+  fi
+  if [ "${TEXT[${cnt}]}" != "" ]; then
+    TEXT[${cnt}]+="
+${TEXT}"
+  else
+    TEXT[${cnt}]="${TEXT}"
+  fi
+done
 
-if [ "${HIT}" != "" -a "${HIT2}" != "" -a "${HIT3}" != "" ]; then
-  if [ "${HIT2}" = "HIT" ]; then
-    HIT2=""
+cnt=1
+for TEXT in "${TEXT[@]}"
+do
+  HIT=`cat "${TEXT}" | grep "${SEARCH_WORD}"`
+  if [ "${SEARCH_WORD2}" != "" ]; then
+    HIT2=`cat "${TEXT}" | grep "${SEARCH_WORD2}"`
+  else
+    HIT2="HIT"
   fi
-  if [ "${HIT3}" = "HIT" ]; then
-    HIT3=""
+  if [ "${SEARCH_WORD3}" != "" ]; then
+    HIT3=`cat "${TEXT}" | grep "${SEARCH_WORD3}"`
+  else
+    HIT3="HIT"
   fi
-  /home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "d swirhen 【log検索 ${DATETIME}】 ${CHANNEL} ログ内で ${SEARCH_WORD} ${SEARCH_WORD2} ${SEARCH_WORD3}にヒットしたよ"
-  /home/swirhen/sh/slack/post.sh "swirhentv" "@here 【log検索 ${DATETIME}】 ${CHANNEL} ログ内で ${SEARCH_WORD} ${SEARCH_WORD2} ${SEARCH_WORD3}にヒットしたよ
+
+  if [ "${HIT}" != "" -a "${HIT2}" != "" -a "${HIT3}" != "" ]; then
+    HIT_STR="${D_T_C_N[${cnt}]} ${TEXT}"
+    /home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "d swirhen 【log検索 ${DATETIME}】 ${CHANNEL} ログ内で ${SEARCH_WORD} ${SEARCH_WORD2} ${SEARCH_WORD3}にヒットしたよ"
+    /home/swirhen/sh/slack/post.sh "swirhentv" "@here 【log検索 ${DATETIME}】 ${CHANNEL} ログ内で ${SEARCH_WORD} ${SEARCH_WORD2} ${SEARCH_WORD3}にヒットしたよ
 \`\`\`
-${HIT}
-${HIT2}
-${HIT3}
+${HIT_STR}
 \`\`\`"
-  ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-sandbox" "@here 【log検索 ${DATETIME}】 ${CHANNEL} ログ内で ${SEARCH_WORD} ${SEARCH_WORD2} ${SEARCH_WORD3}にヒットしたよ
+    ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-sandbox" "@here 【log検索 ${DATETIME}】 ${CHANNEL} ログ内で ${SEARCH_WORD} ${SEARCH_WORD2} ${SEARCH_WORD3}にヒットしたよ
 \`\`\`
-${HIT}
-${HIT2}
-${HIT3}
+${HIT_STR}
 \`\`\`"
-fi
+  fi
+  (( cnt++ ))
+done
 
 sed -i '/^search: ここまで読んだ/d' /data/share/log/${CHANNEL}/${DATE}.txt
 echo "search: ここまで読んだ" >> /data/share/log/${CHANNEL}/${DATE}.txt

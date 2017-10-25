4b97981424afd3b78dd4a693b747e4b1a91d85e9
swirhen <swirhen@gmail.com>
2017-10-26 01:25:23 +0900

    iroiro

--- checker/crawler_check/crawler_check.sh
+++ checker/crawler_check/crawler_check.sh
@@ -27,7 +27,6 @@ fi
 
 if [ "`diff ${FILE} ${FILE}.old`" != "" ]; then
   /home/swirhen/tiasock/tiasock_common.sh "#Twitter@t2" "d swirhen 【汎用URLクロールチェック ${DATE}】 差分あり！ チェックID: ${ID} URL: ${URI}"
-  /home/swirhen/sh/slack/post.sh "swirhentv" "@j_suzuki 【汎用URLクロールチェック ${DATE}】 差分あり！ チェックID: ${ID} URL: ${URI}"
   ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "bot-sandbox" "@here 【汎用URLクロールチェック ${DATE}】 差分あり！ チェックID: ${ID} URL: ${URI}"
 fi
 

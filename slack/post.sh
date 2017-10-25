c670d8409be833506722015de78ea0d8fc421598
swirhen <swirhen@gmail.com>
2017-10-26 01:26:33 +0900

    iro

--- slack/post.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
-CHANNEL=$1
-TEXT=$2
-TOKEN=`cat ${SCRIPT_DIR}/token`
-
-curl -XPOST -d "token=${TOKEN}" -d "channel=${CHANNEL}" -d "text=${TEXT}" -d "username=swirhentv" -d "link_names=1" "https://slack.com/api/chat.postMessage"
--- slack/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
-CHANNEL=$1
-FILENAME=$2
-TITLE=$3
-TOKEN=`cat ${SCRIPT_DIR}/token`
-
-curl -F channels=${CHANNEL} -F file=@"${FILENAME}" -F title=${TITLE} -F token=${TOKEN} https://slack.com/api/files.upload

e25b1268848354bb3349e4de00ede768e4c0f8f8
swirhen <swirhen@gmail.com>
2018-05-15 09:30:32 +0900

    bkup script

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# もろもろバックアップ
+sudo cp -rp /etc/bind/named.conf ~/Dropbox/config/bind/
+sudo cp -rp /etc/bind/named.conf.default-zones ~/Dropbox/config/bind/
+sudo cp -rp /etc/bind/named.conf.local ~/Dropbox/config/bind/
+sudo cp -rp /etc/bind/named.conf.options ~/Dropbox/config/bind/
+sudo cp -rp /etc/bind/swirhen.tv ~/Dropbox/config/bind/
+sudo cp -rp /etc/bind/0.168.192.in-addr.arpa ~/Dropbox/config/bind/
+sudo cp -rp /etc/dhcp/dhcpd.conf ~/Dropbox/config/
+sudo cp -rp /etc/fstab ~/Dropbox/config/
+#sudo cp -rp 

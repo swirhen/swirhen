b95854c828a1aee08ebd3d65104b445bbff2da02
swirhen <swirhen@gmail.com>
2018-05-15 09:33:11 +0900

    bkup script

--- backup.sh
+++ backup.sh
@@ -9,3 +9,4 @@ sudo cp -rp /etc/bind/0.168.192.in-addr.arpa ~/Dropbox/config/bind/
 sudo cp -rp /etc/dhcp/dhcpd.conf ~/Dropbox/config/
 sudo cp -rp /etc/fstab ~/Dropbox/config/
 #sudo cp -rp 
+tar zcvf ~/Dropbox/config/www.tgz /var/www

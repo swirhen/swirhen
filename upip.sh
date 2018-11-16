76fb7dc08920988dc5f643a112f2452c96a15b6e
swirhen <swirhen@gmail.com>
2018-11-16 15:49:04 +0900

    しゅうせい

--- upip.sh
+++ upip.sh
@@ -1,8 +1,2 @@
 wget http://www.luft.co.jp/cgi/ipcheck.php -O /tmp/myip.html
-#wget http://swirhen.net/cgi-bin/ipcheck.cgi -O /tmp/myip.html
-#grep name=\"IP\" /tmp/tmp.html | cut -d" " -f4 | cut -d"\"" -f2 > /tmp/myip.txt
-cp /tmp/myip.html /home/swirhen/Dropbox/temp/
-#ncftpput -u swirhen -p irankae swirhen.net /httpdocs /tmp/myip.html
-#ncftpput -u swirhen -p irankae swirhen.net /httpdocs /tmp/myip.html
-#rm /tmp/myip.html /tmp/tmp.txt
-rm /tmp/myip.html
+mv /tmp/myip.html /home/swirhen/Dropbox/temp/

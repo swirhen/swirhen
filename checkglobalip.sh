0dbe5f2b847cec286c7efd3d77f8ad5c00275897
swirhen <swirhen@gmail.com>
2020-02-02 10:50:06 +0900

    umu

---
+++ checkglobalip.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+IPFILE='/tmp/globalip'
+
+PREVIP=`cat $IPFILE` 2>/dev/null
+IP=`curl globalip.me` 2>/dev/null
+
+echo "`date`"
+if [ "$IP" != "" ]; then
+  echo "Global IP address: $IP"
+
+  if [ "$PREVIP" != "$IP" ]; then
+    echo $IP > $IPFILE
+    if [ "$PREVIP" != "" ]; then
+      echo "Global IP address changed from $PREVIP"
+      echo
+      whois -h whois.nic.ad.jp $IP | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.\S*' | tr '\n' ' '
+      exit 1
+    fi
+  fi
+else
+  echo "Can't get IP address"
+  exit 1
+fi
+
+return 0

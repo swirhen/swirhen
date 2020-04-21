73b11e734adadb78191eb03e25ddd989076249c8
swirhen <swirhen@gmail.com>
2020-04-22 07:40:20 +0900

    debug

--- checkdatadrv.sh
+++ checkdatadrv.sh
@@ -18,13 +18,14 @@ DRIVES=`df -h | grep data`
 DRIVES_NUM=`echo "${DRIVES}" | wc -l`
 
 if [ ${DRIVES_NUM} -ne ${DRIVES_NUM_CORRECT} ]; then
-  TEXT="@channel [ALERT] logical drives mounted not ${DRIVES_NUM_CORRECT} drives."
+  TEXT="@channel [ALERT] logical drives mounted not ${DRIVES_NUM_CORRECT} drives. tring re-mount."
   TEXT+="
 
 DRIVES:
 "
   TEXT+="${DRIVES}"
   slack_post "${TEXT}"
+  sudo mount -a
 elif [ "$1" != "" ]; then
   TEXT="@here [INFO] logical drives mounted ${DRIVES_NUM} drives."
   TEXT+="

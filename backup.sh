dd5fc52b024eefc688d3ccc9e01faebcd51ccc1d
swirhen <swirhen@gmail.com>
2020-05-04 22:59:04 +0900

    debug

--- backup.sh
+++ backup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # もろもろバックアップ
-sudo cp -rp /etc/bind/named.conf ~/Dropbox/config/bind/
-sudo cp -rp /etc/bind/named.conf.default-zones ~/Dropbox/config/bind/
-sudo cp -rp /etc/bind/named.conf.local ~/Dropbox/config/bind/
-sudo cp -rp /etc/bind/named.conf.options ~/Dropbox/config/bind/
-sudo cp -rp /etc/bind/swirhen.tv ~/Dropbox/config/bind/
-sudo cp -rp /etc/bind/0.168.192.in-addr.arpa ~/Dropbox/config/bind/
-sudo cp -rp /etc/dhcp/dhcpd.conf ~/Dropbox/config/
+#sudo cp -rp /etc/bind/named.conf ~/Dropbox/config/bind/
+#sudo cp -rp /etc/bind/named.conf.default-zones ~/Dropbox/config/bind/
+#sudo cp -rp /etc/bind/named.conf.local ~/Dropbox/config/bind/
+#sudo cp -rp /etc/bind/named.conf.options ~/Dropbox/config/bind/
+#sudo cp -rp /etc/bind/swirhen.tv ~/Dropbox/config/bind/
+#sudo cp -rp /etc/bind/0.168.192.in-addr.arpa ~/Dropbox/config/bind/
+#sudo cp -rp /etc/dhcp/dhcpd.conf ~/Dropbox/config/
 sudo cp -rp /etc/fstab ~/Dropbox/config/
-sudo cp -rp /etc/squid/* ~/Dropbox/config/squid/
+#sudo cp -rp /etc/squid/* ~/Dropbox/config/squid/
 #sudo cp -rp 
 tar zcvf ~/Dropbox/config/www.tgz /var/www
 tar zcvf ~/Dropbox/config/ssh.tgz ~/.ssh

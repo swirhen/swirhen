b939445769763c035bfe4c322d41909cd59ce34d
swirhen <swirhen@gmail.com>
2020-05-17 03:43:59 +0900

    mod

--- tigkw.sh
+++ tigkw.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env zsh
-kill `cat /home/swirhen/tig.pid`
+#!/usr/bin/env bash
+sudo docker kill `cat /home/swirhen/TIG/docker.id`
 sleep 5
 cd /home/swirhen/TIG
-#nohup /home/swirhen/TIG/TwitterIrcGateway --BroadcastUpdate=true --port=16668 --encoding=utf-8 --disable-userlist=true --set-topic-onstatuschanged=true --broadcast-update-message-is-notice=true --enable-compression=false --disable-notice-at-first-time=true --OAuthClientKey=Cjx5NIRxdYKuTbKZbZHfR1AVM --OAuthSecretKey=rYHeaHLwnaFOC3XG2qwcyy3yvHNSKvpJRjfHHcqDQqWB5ZZwVl  --bind-address=0.0.0.0 &
-nohup /home/swirhen/TIG/TwitterIrcGateway --BroadcastUpdate=true --port=16668 --encoding=utf-8 --disable-userlist=true --set-topic-onstatuschanged=true --broadcast-update-message-is-notice=true --enable-compression=false --disable-notice-at-first-time=true --OAuthClientKey=ZHt8Txf7vmHmWr4PzNQ5okFgf --OAuthSecretKey=f7QdBhK8GVsIIHyO8GQjEJ44OxG3m6p7lNjQ2zZXYlHpFIRV91 --bind-address=0.0.0.0 &
+sudo docker run -d --rm -v /home/swirhen/TIG/:/app/ -p 16668:16668 mono:4.8 /bin/sh -c 'mono /app/TwitterIrcGatewayCLI.exe --BroadcastUpdate=true --port=16668 --encoding=utf-8 --disable-userlist=true --set-topic-onstatuschanged=true --broadcast-update-message-is-notice=true --enable-compression=false --disable-notice-at-first-time=true --OAuthClientKey=ZHt8Txf7vmHmWr4PzNQ5okFgf --OAuthSecretKey=f7QdBhK8GVsIIHyO8GQjEJ44OxG3m6p7lNjQ2zZXYlHpFIRV91 --bind-address=0.0.0.0' > docker.id
+#nohup /home/swirhen/TIG/TwitterIrcGateway --BroadcastUpdate=true --port=16668 --encoding=utf-8 --disable-userlist=true --set-topic-onstatuschanged=true --broadcast-update-message-is-notice=true --enable-compression=false --disable-notice-at-first-time=true --OAuthClientKey=ZHt8Txf7vmHmWr4PzNQ5okFgf --OAuthSecretKey=f7QdBhK8GVsIIHyO8GQjEJ44OxG3m6p7lNjQ2zZXYlHpFIRV91 --bind-address=0.0.0.0 &

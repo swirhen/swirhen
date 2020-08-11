29be4d77e0ad6daea33c49ce206fe0823c49d0dd
swirhen <swirhen@gmail.com>
2020-08-11 12:25:13 +0900

    check mysql service

---
+++ checkmysqlservice.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+PYTHON_PATH="python3"
+CHANNEL="bot-open"
+
+slack_post() {
+    ${PYTHON_PATH} /home/swirhen/sh/slackbot/swirhentv/post.py "${CHANNEL}" "$1"
+}
+
+echo "`date`"
+mysqllive=`systemctl status mysql | grep Active | grep running`
+
+if [ "${mysqllive}" != "" ]; then
+    echo "mysql live."
+else
+    error=0
+    until [ "${mysqllive}" != "" ];
+    do
+        (( error++ ))
+        if [ $error -gt 5 ]; then
+            break;
+        fi
+        sudo systemctl restart mysql
+        mysqllive=`systemctl status mysql | grep Active | grep running`
+    done
+    if [ $error -gt 5 ]; then
+        TEXT="@channel [ALERT] mysql service has gone and restart failed."
+        slack_post "${TEXT}"
+    elif [ "$1" != "" ]; then
+        /home/swirhen/sh/tiarrakw.sh
+        TEXT="@here [INFO] mysql service has gone. restart ok and tiarra restarted."
+        slack_post "${TEXT}"
+    fi
+fi
+
+return 0

73491244ad2189a5eef937c9d8a0ffe95c2f5abd
swirhen <irankae1#>
2021-05-13 14:26:39 +0900

    debug

--- checker/torrentsearch/torrentsearch.py
+++ checker/torrentsearch/torrentsearch.py
@@ -3,13 +3,7 @@
 # torrent file 汎用クロール
 # リストに指定したキーワードでnyaaおよびsukebei.nyaaをクロールし、ヒットしたらseedをダウンロードしておく
 # import section
-import datetime
-import pprint
-
 import git
-import glob
-import math
-import os
 import pathlib
 import re
 import sys
@@ -29,9 +23,9 @@ TDATETIME = dt.now()
 DATETIME = TDATETIME.strftime('%Y/%m/%d %H:%M:%S')
 DATE = TDATETIME.strftime('%Y%m%d')
 DOWNLOAD_DIR = f'/data/share/temp/torrentsearch/{DATE}'
+SLACK_CHANNEL = 'torrent-search'
 
-
-# すべてのフィードリスト取得(ループ親)
+# すべてのフィード取得(ループ親)
 def get_seed_list():
     # URLリストをループして、すべてのURLから最新フィードを取得
     # カテゴリ名,title,linkを配列に入れる
@@ -44,7 +38,7 @@ def get_seed_list():
 
     return all_seed_list
 
-# フィードリスト取得(URLから)
+# フィード取得(URLから)
 def get_seed_list_proc(category, feed_uri):
     seed_list = []
     req = urllib.request.Request(feed_uri)
@@ -61,9 +55,49 @@ def get_seed_list_proc(category, feed_uri):
 
 
 if __name__ == '__main__':
-    hit_flag = 0
-    check_list = []
-
+    # フィード取得
     seedlist = get_seed_list()
 
-    pprint.pprint(seedlist)
+    # チェックリスト取得
+    check_list = []
+    for checkitem in list(open(CHECKLIST_FILE).read().splitlines()):
+        check_category = checkitem.split('|')[0]
+        check_keyword = checkitem.split('|')[1]
+        check_list.append([check_category, check_keyword])
+
+    hit_flag = 0
+    hit_result = []
+    # カテゴリと一致、キーワードと一致、URLリスト内に存在しない場合、ダウンロードしてリストに加える
+    for seed_item in seedlist:
+        item_category = seed_item[0]
+        item_title = seed_item[1]
+        item_link = seed_item[2]
+
+        for check_item in check_list:
+            check_category = check_item[0]
+            check_keyword = check_item[1]
+            if item_category == check_category and \
+                re.search(check_keyword, item_title) and \
+                swiutil.grep_file(DL_URL_LIST_FILE, item_link) == '':
+                hit_flag = 1
+                hit_result.append([item_category, item_title, check_keyword])
+                urllib.request.urlretrieve(item_link, f'{DOWNLOAD_DIR}/{item_title}.torrent')
+                swiutil.writefile_append(DL_URL_LIST_FILE, item_link)
+
+    if hit_flag == 1:
+        post_str = f'@here 【swirhen.tv 汎用種調査 {DATETIME}】キーワードヒット: ダウンロードしました\n```# 結果\n'
+        for result_item in hit_result:
+            post_str += f'カテゴリ: {result_item[0]} キーワード: {result_item[2]} タイトル: {result_item[1]}\n'
+
+        post_str += '# ダウンロードしたseedファイル\n'
+        for result_item in hit_result:
+            post_str += f'{result_item[1]}.torrent\n'
+
+        swiutil.slack_post(SLACK_CHANNEL, post_str)
+
+        repo = git.Repo(SCRIPT_DIR)
+        repo.git.commit(CHECKLIST_FILE, message='checklist.txt update')
+        repo.git.commit(DL_URL_LIST_FILE, message='download_url.txt update')
+        repo.git.pull()
+        repo.git.push()
+

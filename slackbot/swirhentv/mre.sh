1e28f07b2c9c149d70af8337b38fcb8f6ce350b1
swirhen <irankae1#>
2021-05-24 10:31:23 +0900

    suteru

--- slackbot/swirhentv/mre.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-cd /data/share/movie
-/data/share/movie/sh/mre.sh
--- slackbot/swirhentv/rmm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-cd /data/share/movie
-echo "`date '+%Y/%m/%d %H:%M:%S'` rm *.torrent"
-rm *.torrent
-echo "`date '+%Y/%m/%d %H:%M:%S'` movie files rename start."
-/data/share/movie/sh/mre.sh
-echo "`date '+%Y/%m/%d %H:%M:%S'` movie files rename end."

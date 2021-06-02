b57a8c1dd29f9928f4d0bf5955924b3e0b61ba55
swirhen <irankae1#>
2021-06-02 13:10:47 +0900

    use db

--- checker/torrentsearch/torrentsearch.py
+++ checker/torrentsearch/torrentsearch.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # torrent file 汎用クロール
-# リストに指定したキーワードでnyaaおよびsukebei.nyaaをクロールし、ヒットしたらseedをダウンロードしておく
+# リストに指定したキーワードでnyaaおよびsukebei.nyaaをクロールしたデータベースを検索し、ヒットしたらseedをダウンロードしておく
 # import section
 import os
 import git
 import pathlib
-import re
 import sys
 import urllib.request
 from datetime import datetime as dt
-import xml.etree.ElementTree as elementTree
 current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append('/data/share/movie/sh/python-lib/')
 import swirhentv_util as swiutil
+import sqlite3
 
 # arguments section
 GIT_ROOT_DIR = '/home/swirhen/sh'
@@ -21,48 +20,36 @@ SCRIPT_DIR = str(current_dir)
 CHECKLIST_FILE = f'{SCRIPT_DIR}/checklist.txt'
 URL_LIST_FILE = f'{SCRIPT_DIR}/urllist.txt'
 DL_URL_LIST_FILE = f'{SCRIPT_DIR}/download_url.txt'
+LAST_CHECK_DATE_FILE = f'{SCRIPT_DIR}/last_check_date.txt'
+FEED_DB = f'{SCRIPT_DIR}/nyaatorrent_feed.db'
+CONN = sqlite3.connect(FEED_DB)
+CUR = CONN.cursor()
 TDATETIME = dt.now()
 DATETIME = TDATETIME.strftime('%Y/%m/%d %H:%M:%S')
 DATE = TDATETIME.strftime('%Y%m%d')
 DOWNLOAD_DIR = f'/data/share/temp/torrentsearch/{DATE}'
 SLACK_CHANNEL = 'torrent-search'
 
-# すべてのフィード取得(ループ親)
-def get_seed_list(category='all'):
-    # URLリストをループして、すべてのURLから最新フィードを取得
-    # カテゴリ名,title,linkを配列に入れる
-    all_seed_list = []
-    with open(URL_LIST_FILE) as f:
-        for line in list(f.read().splitlines()):
-            cat = line.split()[0]
-            url = line.split()[1]
-            if category == 'all' or category == cat:
-                seed_list = get_seed_list_proc(cat, url)
-                all_seed_list.extend(seed_list)
-
-    return all_seed_list
-
-# フィード取得(URLから)
-def get_seed_list_proc(category, feed_uri):
-    seed_list = []
-    req = urllib.request.Request(feed_uri)
-    with urllib.request.urlopen(req) as response:
-        xml_string = response.read()
-
-    xml_root = elementTree.fromstring(xml_string)
-
-    for item in xml_root.findall('./channel/item'):
-        seed_info = [category, item.find('title').text, item.find('link').text, item.find('pubDate').text[:-6]]
-        seed_list.append(seed_info)
-
-    return seed_list
+# nyaa データベース検索
+def search_seed_list(category, keyword, last_check_date):
+    select_sql = 'select title, link' \
+                 ' from feed_data' \
+                f' where category = "{category}"' \
+                f' and title like "%{keyword}%"' \
+                f' and created_at > "{last_check_date}"'
+    result = list(CUR.execute(select_sql))
+    return result
 
 
 if __name__ == '__main__':
-    # フィード取得
-    seedlist = get_seed_list()
+    # 最終取得時刻
+    with open(LAST_CHECK_DATE_FILE) as file:
+        last_check_date = file.read().splitlines()[0]
+    # 今回の取得時刻
+    now_date = dt.now().strftime('%Y-%m-%d %H:%M')
+    swiutil.writefile_new(LAST_CHECK_DATE_FILE, now_date)
 
-    # チェックリスト取得
+    # チェックリスト取得(カテゴリごとのキーワード配列)
     check_list = dict()
     with open(CHECKLIST_FILE) as file:
         for checkitem in list(file.read().splitlines()):
@@ -73,26 +60,26 @@ if __name__ == '__main__':
             check_keyword = checkitem.split('|')[1]
             check_list[check_category].append(check_keyword)
 
-    hit_flag = 0
+    hit_flag = False
     hit_result = []
-    # カテゴリでキーワードリスト検索、キーワードと一致、URLリスト内に存在しない場合、ダウンロードしてリストに加える
-    for seed_item in seedlist:
-        item_category = seed_item[0]
-        item_title = seed_item[1]
-        item_link = seed_item[2]
+    # カテゴリ、キーワードでキーワードリスト検索、URLリスト内に存在しない場合、ダウンロードしてリストに加える
+    for check_category in check_list:
+        for check_keyword in check_list[check_category]:
+            search_result = search_seed_list(check_category, check_keyword, last_check_date)
+            if len(search_result) > 0:
+                for search_item in search_result:
+                    item_title = search_item[0]
+                    item_link = search_item[1]
+                    if len(swiutil.grep_file2(DL_URL_LIST_FILE, item_link, True)) == 0:
+                        hit_flag = True
+                        if not os.path.isdir(DOWNLOAD_DIR):
+                            os.mkdir(DOWNLOAD_DIR)
+                        item_title = swiutil.truncate(item_title.translate(str.maketrans('/;!','___')), 247)
+                        hit_result.append([check_category, item_title, check_keyword])
+                        urllib.request.urlretrieve(item_link, f'{DOWNLOAD_DIR}/{item_title}.torrent')
+                        swiutil.writefile_append(DL_URL_LIST_FILE, item_link)
 
-        for check_keyword in check_list[item_category]:
-            if re.search(check_keyword, item_title) and \
-                len(swiutil.grep_file(DL_URL_LIST_FILE, item_link)) == 0:
-                hit_flag = 1
-                if not os.path.isdir(DOWNLOAD_DIR):
-                    os.mkdir(DOWNLOAD_DIR)
-                item_title = swiutil.truncate(item_title.translate(str.maketrans('/;!','___')), 247)
-                hit_result.append([item_category, item_title, check_keyword])
-                urllib.request.urlretrieve(item_link, f'{DOWNLOAD_DIR}/{item_title}.torrent')
-                swiutil.writefile_append(DL_URL_LIST_FILE, item_link)
-
-    if hit_flag == 1:
+    if hit_flag:
         post_str = f'@here 【swirhen.tv 汎用種調査 {DATETIME}】キーワードヒット: ダウンロードしました\n```# 結果\n'
         for result_item in hit_result:
             post_str += f'カテゴリ: {result_item[0]} キーワード: {result_item[2]} タイトル: {result_item[1]}\n'

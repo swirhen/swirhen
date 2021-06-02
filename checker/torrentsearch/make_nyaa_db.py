4eb0d3dbf989fea3ee458fddbd7fdf0162e7ee2a
swirhen <irankae1#>
2021-06-02 11:50:29 +0900

    nyaa db

--- checker/torrentsearch/make_nyaa_db.py
+++ checker/torrentsearch/make_nyaa_db.py
@@ -3,18 +3,92 @@
 # nyaatorrent make feed db
 # import section
 import pathlib
-import subprocess
 import sys
+import urllib.request
 import sqlite3
-import swirhentv_util as swiutil
+import xml.etree.ElementTree as elementTree
 
 # argment section
 current_dir = pathlib.Path(__file__).resolve().parent
 SCRIPT_DIR = str(current_dir)
+URL_LIST_FILE = f'{SCRIPT_DIR}/urllist.txt'
 FEED_DB = f'{SCRIPT_DIR}/nyaatorrent_feed.db'
 
 
-def make_nyaa_data():
-    
-    
+# すべてのフィード取得(ループ親)
+def get_seed_list(category='all'):
+    # URLリストをループして、すべてのURLから最新フィードを取得
+    # カテゴリ名,title,linkを配列に入れる
+    all_seed_list = []
+    with open(URL_LIST_FILE) as f:
+        for line in list(f.read().splitlines()):
+            cat = line.split()[0]
+            url = line.split()[1]
+            if category == 'all' or category == cat:
+                seed_list = get_seed_list_proc(cat, url)
+                all_seed_list.extend(seed_list)
+
+    return all_seed_list
+
+
+# フィード取得(URLから)
+def get_seed_list_proc(category, feed_uri):
+    seed_list = []
+    req = urllib.request.Request(feed_uri)
+    with urllib.request.urlopen(req) as response:
+        xml_string = response.read()
+
+    xml_root = elementTree.fromstring(xml_string)
+
+    for item in xml_root.findall('./channel/item'):
+        seed_info = [category, item.find('title').text, item.find('link').text, item.find('pubDate').text[:-6]]
+        seed_list.append(seed_info)
+
+    return seed_list
+
+
+def make_nyaa_data(category='all'):
+    # URLリストをループして、すべてのURLから最新フィードを取得
+    # カテゴリ名,title,link,pubDateを配列に入れる
+    seed_list = []
+    with open(URL_LIST_FILE) as f:
+        for line in list(f.read().splitlines()):
+            cat = line.split()[0]
+            url = line.split()[1]
+            if category == 'all' or category == cat:
+                seed_list = get_seed_list_proc(cat, url)
+                seed_list.extend(seed_list)
+
+    conn = sqlite3.connect(FEED_DB)
+    cur = conn.cursor()
+    # 参考SQL
+    drop_table_sql = 'drop table if exists feed_data'
+    create_table_sql = 'create table if not exists feed_data(' \
+                        ' category string,' \
+                        ' title string,' \
+                        ' link string unique,' \
+                        ' pubdate timestamp,' \
+                        ' created_at timestamp default (datetime(\'now\', \'localtime\')))'
+    delete_record_sql = 'delete from feed_data where category'
+
+    values = []
+    for seed_item in seed_list:
+        item_category = seed_item[0]
+        item_title = seed_item[1]
+        item_link = seed_item[2]
+        item_pubdate = seed_item[3]
+        values.append(f'("{item_category}", "{item_title}", "{item_link}", "{item_pubdate}")')
+    values_str = ', '.join(values)
+    insert_sql = 'inser into feed_data(category, title, link, pubdate)' \
+                f' values{values_str}' \
+                 ' conflict(link) do nothing'
+    cur.execute(insert_sql)
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
 

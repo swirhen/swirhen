0015e61afb709200cb47ae7461a263fcd78facfb
swirhen <swirhen@gmail.com>
2021-06-04 11:41:25 +0900

    backup tuika

--- backup.sh
+++ backup.sh
@@ -6,9 +6,8 @@ sudo cp -rp /etc/bind/named.conf.local ~/Dropbox/config/bind/
 sudo cp -rp /etc/bind/named.conf.options ~/Dropbox/config/bind/
 sudo cp -rp /etc/bind/swirhen.tv ~/Dropbox/config/bind/
 sudo cp -rp /etc/bind/0.168.192.in-addr.arpa ~/Dropbox/config/bind/
-#sudo cp -rp /etc/dhcp/dhcpd.conf ~/Dropbox/config/
 sudo cp -rp /etc/fstab ~/Dropbox/config/
-#sudo cp -rp /etc/squid/* ~/Dropbox/config/squid/
+sudo cp -p /home/swirhen/sh/checker/torrentsearch/nyaatorrent_feed.db ~/Dropbox/sqlite_db_backup/
 #sudo cp -rp 
 tar zcvf ~/Dropbox/config/www.tgz /var/www
 tar zcvf ~/Dropbox/config/ssh.tgz ~/.ssh

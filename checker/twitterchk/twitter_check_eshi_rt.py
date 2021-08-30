cf52c4157aedc0df35fe77ab70ef5de86f000859
swirhen <swirhen@gmail.com>
2021-08-30 22:28:38 +0900

    debug

--- checker/twitterchk/twitter_check_eshi_rt.py
+++ checker/twitterchk/twitter_check_eshi_rt.py
@@ -100,7 +100,7 @@ for log in logs[CHECK_CHANNEL]:
         tweets = text.replace('\n','_').split('♻')
         for tweet in tweets:
             if re.search(' RT @', tweet) and \
-                re.search(r'#soraArt|#ロボ子Art|#miko_Art|#ほしまちぎゃらりー|#メルArt|#アロ絵|#はあとart|#絵フブキ|#祭絵|#あくあーと|#シオンの書物|#百鬼絵巻|#しょこらーと|#プロテインザスバル|#みおーん絵|#絵かゆ|#できたてころね|#AZKiART|#ぺこらーと|#絵クロマンサー|#しらぬえ|#ノエラート|#マリンのお宝|#かなたーと|#みかじ絵|#つのまきあーと|#TOWART|#ルーナート|#LamyArt|#ねねアルバム|#ししらーと|#絵まる|#GambaRisu|#ioarts|#HoshinovArt|#anyatelier|#Reinessance|#graveyART|#絵ニックス|#callillust|#ameliaRT|#いなート|#gawrt|#inART|#artsofashes|#teamates|#callioP|#スケベなアロ絵|#肌色まつり|#まつりは絵っち|#エロおにぎり|#オークアート|#沈没後悔日記|#glAMErous|#IRySart|#illustrayBAE|#ベーアート|#faunline|#kronillust|#galaxillust|#クロニーラスト|#holoCouncil|#omegallery|#drawMEI|#ムメ絵', tweet):
+                re.search(r'#soraArt|#ロボ子Art|#miko_Art|#ほしまちぎゃらりー|#メルArt|#アロ絵|#はあとart|#絵フブキ|#祭絵|#あくあーと|#シオンの書物|#百鬼絵巻|#しょこらーと|#プロテインザスバル|#みおーん絵|#絵かゆ|#できたてころね|#AZKiART|#ぺこらーと|#絵クロマンサー|#しらぬえ|#ノエラート|#マリンのお宝|#かなたーと|#みかじ絵|#つのまきあーと|#TOWART|#ルーナート|#LamyArt|#ねねアルバム|#ししらーと|#絵まる|#GambaRisu|#ioarts|#HoshinovArt|#anyatelier|#Reinessance|#graveyART|#絵ニックス|#callillust|#ameliaRT|#いなート|#gawrt|#inART|#artsofashes|#teamates|#callioP|#スケベなアロ絵|#肌色まつり|#まつりは絵っち|#エロおにぎり|#オークアート|#沈没後悔日記|#glAMErous|#IRySart|#illustrayBAE|#ベーアート|#faunline|#kronillust|#galaxillust|#クロニーラスト|#holoCouncil|#omegallery|#drawMEI|#ムメ絵|#FineFaunart', tweet):
                 rt_nick = re.sub(r'.*RT\ @(.*?):.*', r'\1', tweet)
                 if len(swiutil.grep_file(CHECKLIST_FILE, rt_nick)) == 0:
                     result.append(f'リストに無いホロ絵師ID({rt_nick})がRTされたのでリスト追加:\n[{date}] <{nick}> ♻{tweet.replace("_"," ")}')

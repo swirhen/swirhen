3ef47007c79e5a736941d0ef72711aacd2edc1f9
swirhen <irankae1#>
2022-04-28 13:08:17 +0900

    ladder add

--- checker/diablo_clone_tracker/diablo_clone_tracker.py
+++ checker/diablo_clone_tracker/diablo_clone_tracker.py
@@ -20,8 +20,12 @@ SCRIPT_DIR = str(current_dir)
 PROGRESS_ASIA = f'{SCRIPT_DIR}/asia.txt'
 PROGRESS_US = f'{SCRIPT_DIR}/us.txt'
 PROGRESS_EU = f'{SCRIPT_DIR}/eu.txt'
+PROGRESS_ASIA_L = f'{SCRIPT_DIR}/asia_l.txt'
+PROGRESS_US_L = f'{SCRIPT_DIR}/us_l.txt'
+PROGRESS_EU_L = f'{SCRIPT_DIR}/eu_l.txt'
 DISCORD_CHANNEL = 'diablo-clone-tracker'
 DISCORD_CHANNEL2 = 'diablo-clone-tracker-pub'
+DISCORD_CHANNEL2L = 'diablo-clone-tracker-pub_l'
 
 # main module
 def main(force_flg=False):
@@ -37,6 +41,18 @@ def main(force_flg=False):
         p_us = f.readline()[0]
     with open(PROGRESS_EU) as f:
         p_eu = f.readline()[0]
+    asia_chg_flg_l = False
+    us_chg_flg_l = False
+    eu_chg_flg_l = False
+    asia_add_flg_l = False
+    us_add_flg_l = False
+    eu_add_flg_l = False
+    with open(PROGRESS_ASIA_L) as f:
+        p_asia_l = f.readline()[0]
+    with open(PROGRESS_US_L) as f:
+        p_us_l = f.readline()[0]
+    with open(PROGRESS_EU_L) as f:
+        p_eu_l = f.readline()[0]
 
     req = urllib.request.Request(API_URI)
     try:
@@ -49,27 +65,50 @@ def main(force_flg=False):
 
     if len(dict_from_api) > 0:
         for item in dict_from_api:
-            if item['region'] == '1':
-                n_us = item['progress']
-                if n_us != p_us:
-                    swiutil.writefile_new(PROGRESS_US, n_us)
-                    us_chg_flg = True
-                if n_us > p_us:
-                    us_add_flg = True
-            elif item['region'] == '2':
-                n_eu = item['progress']
-                if n_eu != p_eu:
-                    swiutil.writefile_new(PROGRESS_EU, n_eu)
-                    eu_chg_flg = True
-                if n_eu > p_eu:
-                    eu_add_flg = True
-            elif item['region'] == '3':
-                n_asia = item['progress']
-                if n_asia != p_asia:
-                    swiutil.writefile_new(PROGRESS_ASIA, n_asia)
-                    asia_chg_flg = True
-                if n_asia > p_asia:
-                    asia_add_flg = True
+            if item['ladder'] == '2':
+                if item['region'] == '1':
+                    n_us = item['progress']
+                    if n_us != p_us:
+                        swiutil.writefile_new(PROGRESS_US, n_us)
+                        us_chg_flg = True
+                    if n_us > p_us:
+                        us_add_flg = True
+                elif item['region'] == '2':
+                    n_eu = item['progress']
+                    if n_eu != p_eu:
+                        swiutil.writefile_new(PROGRESS_EU, n_eu)
+                        eu_chg_flg = True
+                    if n_eu > p_eu:
+                        eu_add_flg = True
+                elif item['region'] == '3':
+                    n_asia = item['progress']
+                    if n_asia != p_asia:
+                        swiutil.writefile_new(PROGRESS_ASIA, n_asia)
+                        asia_chg_flg = True
+                    if n_asia > p_asia:
+                        asia_add_flg = True
+            elif item['ladder'] == '1':
+                if item['region'] == '1':
+                    n_us_l = item['progress']
+                    if n_us_l != p_us_l:
+                        swiutil.writefile_new(PROGRESS_US_L, n_us_l)
+                        us_chg_flg_l = True
+                    if n_us_l > p_us_l:
+                        us_add_flg_l = True
+                elif item['region'] == '2':
+                    n_eu_l = item['progress']
+                    if n_eu_l != p_eu_l:
+                        swiutil.writefile_new(PROGRESS_EU_L, n_eu_l)
+                        eu_chg_flg_l = True
+                    if n_eu_l > p_eu:_l
+                        eu_add_flg_l = True
+                elif item['region'] == '3':
+                    n_asia_l = item['progress']
+                    if n_asia_l != p_asia_l:
+                        swiutil.writefile_new(PROGRESS_ASIA_L, n_asia_l)
+                        asia_chg_flg_l = True
+                    if n_asia_l > p_asia_l:
+                        asia_add_flg_l = True
 
     if force_flg or us_chg_flg or eu_chg_flg or asia_chg_flg:
         if us_add_flg or eu_add_flg or asia_add_flg:
@@ -97,6 +136,34 @@ def main(force_flg=False):
 
         swiutil.discord_post(DISCORD_CHANNEL, post_str)
         swiutil.discord_post(DISCORD_CHANNEL2, post_str)
+
+
+    if force_flg or us_chg_flg_l or eu_chg_flg_l or asia_chg_flg_l:
+        if us_add_flg_l or eu_add_flg_l or asia_add_flg_l:
+            post_str = '@here 【diablo2.io diablo clone tracker(ladder)】'
+        else:
+            post_str = '【diablo2.io diablo clone tracker(ladder)】'
+
+        if force_flg:
+            post_str += '(定時チェック)\n'
+        else:
+            post_str += '\n'
+
+        if asia_chg_flg_l:
+            post_str += f'アジア(**変更あり！**)：{p_asia_l} -> {n_asia_l}\n'
+        else:
+            post_str += f'アジア：{n_asia_l}\n'
+        if us_chg_flg_l:
+            post_str += f'US(**変更あり！**)：{p_us_l} -> {n_us_l}\n'
+        else:
+            post_str += f'US：{n_us_l}\n'
+        if eu_chg_flg_l:
+            post_str += f'EU(**変更あり！**)：{p_eu_l} -> {n_eu_l}\n'
+        else:
+            post_str += f'EU：{n_eu_l}\n'
+
+        swiutil.discord_post(DISCORD_CHANNEL2L, post_str)
+
 
 if __name__ == "__main__":
     # main section

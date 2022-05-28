52a2a3083798243a25c9e782e74e541d55160e88
swirhen <irankae1#>
2022-05-28 11:51:28 +0900

    debug

--- checker/diablo_clone_tracker/diablo_clone_tracker.py
+++ checker/diablo_clone_tracker/diablo_clone_tracker.py
@@ -3,6 +3,7 @@
 # diablo2 resurrected diablo clone tracker from diablo2.io
 # diablo2.io の diablo clone tracker apiを取得し、変化があったら通知する
 # import section
+from audioop import add
 import os
 import pathlib
 import sys
@@ -26,10 +27,39 @@ PROGRESS_EU_L = f'{SCRIPT_DIR}/eu_l.txt'
 H_PROGRESS_ASIA_L = f'{SCRIPT_DIR}/asia_l_h.txt'
 H_PROGRESS_US_L = f'{SCRIPT_DIR}/us_l_h.txt'
 H_PROGRESS_EU_L = f'{SCRIPT_DIR}/eu_l_h.txt'
+ADDFLAG_ASIA = f'{SCRIPT_DIR}/asia.add'
+ADDFLAG_US = f'{SCRIPT_DIR}/us.add'
+ADDFLAG_EU = f'{SCRIPT_DIR}/eu.add'
+ADDFLAG_ASIA_L = f'{SCRIPT_DIR}/asia_l.add'
+ADDFLAG_US_L = f'{SCRIPT_DIR}/us_l.add'
+ADDFLAG_EU_L = f'{SCRIPT_DIR}/eu_l.add'
+H_ADDFLAG_ASIA_L = f'{SCRIPT_DIR}/asia_l_h.add'
+H_ADDFLAG_US_L = f'{SCRIPT_DIR}/us_l_h.add'
+H_ADDFLAG_EU_L = f'{SCRIPT_DIR}/eu_l_h.add'
 DISCORD_CHANNEL = 'diablo-clone-tracker'
 DISCORD_CHANNEL2 = 'diablo-clone-tracker-pub'
 DISCORD_CHANNEL2L = 'diablo-clone-tracker-pub_l'
 DISCORD_CHANNEL2HL = 'diablo-clone-tracker-pub_l_h'
+
+# add check process module
+# return add flag
+def add_check(p_val, n_val, flag_file):
+    if p_val < n_val:
+        # 増加
+        if os.path.isfile(flag_file):
+            # フラグファイルがある→Trueを返す
+            return True
+        else:
+            # フラグファイルがない→フラグファイルを作成
+            swiutil.writefile_new(flag_file, '1')
+    elif p_val > n_val:
+        # 減少
+        if os.path.isfile(flag_file):
+            # フラグファイルがある→フラグファイルを消す
+            swiutil.deletefile(flag_file)
+    
+    return False
+
 
 # main module
 def main(force_flg=False):
@@ -88,93 +118,62 @@ def main(force_flg=False):
                         if n_us != p_us:
                             swiutil.writefile_new(PROGRESS_US, n_us)
                             us_chg_flg = True
-                        if n_us > p_us:
-                            us_add_flg = True
+                        us_add_flg = add_check(p_us, n_us, ADDFLAG_US)                        
                     elif item['region'] == '2':
                         n_eu = item['progress']
                         if n_eu != p_eu:
                             swiutil.writefile_new(PROGRESS_EU, n_eu)
                             eu_chg_flg = True
-                        if n_eu > p_eu:
-                            eu_add_flg = True
+                        eu_add_flg = add_check(p_eu, n_eu, ADDFLAG_EU)                        
                     elif item['region'] == '3':
                         n_asia = item['progress']
                         if n_asia != p_asia:
                             swiutil.writefile_new(PROGRESS_ASIA, n_asia)
                             asia_chg_flg = True
-                        if n_asia > p_asia:
-                            asia_add_flg = True
+                        asia_add_flg = add_check(p_asia, n_asia, ADDFLAG_ASIA)                        
                 elif item['ladder'] == '1':
                     if item['region'] == '1':
                         n_us_l = item['progress']
                         if n_us_l != p_us_l:
                             swiutil.writefile_new(PROGRESS_US_L, n_us_l)
                             us_chg_flg_l = True
-                        if n_us_l > p_us_l:
-                            us_add_flg_l = True
+                        us_add_flg_l = add_check(p_us_l, n_us_l, ADDFLAG_US_L)
                     elif item['region'] == '2':
                         n_eu_l = item['progress']
                         if n_eu_l != p_eu_l:
                             swiutil.writefile_new(PROGRESS_EU_L, n_eu_l)
                             eu_chg_flg_l = True
-                        if n_eu_l > p_eu_l:
-                            eu_add_flg_l = True
+                        eu_add_flg_l = add_check(p_eu_l, n_eu_l, ADDFLAG_EU_L)
                     elif item['region'] == '3':
                         n_asia_l = item['progress']
                         if n_asia_l != p_asia_l:
                             swiutil.writefile_new(PROGRESS_ASIA_L, n_asia_l)
                             asia_chg_flg_l = True
-                        if n_asia_l > p_asia_l:
-                            asia_add_flg_l = True
+                        asia_add_flg_l = add_check(p_asia_l, n_asia_l, ADDFLAG_ASIA_L)
             elif item['hc'] == '1':
-                # if item['ladder'] == '2':
-                #     if item['region'] == '1':
-                #         h_n_us = item['progress']
-                #         if h_n_us != h_p_us:
-                #             swiutil.writefile_new(H_PROGRESS_US, h_n_us)
-                #             us_chg_flg_h = True
-                #         if h_n_us > h_p_us:
-                #             us_add_flg_h = True
-                #     elif item['region'] == '2':
-                #         h_n_eu = item['progress']
-                #         if h_n_eu != h_p_eu:
-                #             swiutil.writefile_new(H_PROGRESS_EU, h_n_eu)
-                #             eu_chg_flg_h = True
-                #         if h_n_eu > h_p_eu:
-                #             eu_add_flg_h = True
-                #     elif item['region'] == '3':
-                #         h_n_asia = item['progress']
-                #         if h_n_asia != h_p_asia:
-                #             swiutil.writefile_new(H_PROGRESS_ASIA, h_n_asia)
-                #             asia_chg_flg_h = True
-                #         if h_n_asia > h_p_asia:
-                #             asia_add_flg_h = True
                 if item['ladder'] == '1':
                     if item['region'] == '1':
                         h_n_us_l = item['progress']
                         if h_n_us_l != h_p_us_l:
                             swiutil.writefile_new(H_PROGRESS_US_L, h_n_us_l)
                             us_chg_flg_h_l = True
-                        if h_n_us_l > h_p_us_l:
-                            us_add_flg_h_l = True
+                        us_add_flg_h_l = add_check(h_p_us_l, h_n_us_l, H_ADDFLAG_US_L)
                     elif item['region'] == '2':
                         h_n_eu_l = item['progress']
                         if h_n_eu_l != h_p_eu_l:
                             swiutil.writefile_new(H_PROGRESS_EU_L, h_n_eu_l)
                             eu_chg_flg_h_l = True
-                        if h_n_eu_l > h_p_eu_l:
-                            eu_add_flg_h_l = True
+                        eu_add_flg_h_l = add_check(h_p_eu_l, h_n_eu_l, H_ADDFLAG_EU_L)
                     elif item['region'] == '3':
                         h_n_asia_l = item['progress']
                         if h_n_asia_l != h_p_asia_l:
                             swiutil.writefile_new(H_PROGRESS_ASIA_L, h_n_asia_l)
                             asia_chg_flg_h_l = True
-                        if h_n_asia_l > h_p_asia_l:
-                            asia_add_flg_h_l = True
+                        asia_add_flg_h_l = add_check(h_p_asia_l, h_n_asia_l, H_ADDFLAG_ASIA_L)
 
     if force_flg or us_chg_flg or eu_chg_flg or asia_chg_flg:
         if us_add_flg or eu_add_flg or asia_add_flg:
-            post_str = '@here 【diablo2.io diablo clone tracker】'
+            post_str = '@here 【diablo2.io diablo clone tracker】(連続の増加を検知)'
         else:
             post_str = '【diablo2.io diablo clone tracker】'
 
@@ -202,7 +201,7 @@ def main(force_flg=False):
 
     if force_flg or us_chg_flg_l or eu_chg_flg_l or asia_chg_flg_l:
         if us_add_flg_l or eu_add_flg_l or asia_add_flg_l:
-            post_str = '@here 【diablo2.io diablo clone tracker(ladder)】'
+            post_str = '@here 【diablo2.io diablo clone tracker(ladder)】(連続の増加を検知)'
         else:
             post_str = '【diablo2.io diablo clone tracker(ladder)】'
 
@@ -229,7 +228,7 @@ def main(force_flg=False):
 
     if force_flg or us_chg_flg_h_l or eu_chg_flg_h_l or asia_chg_flg_h_l:
         if us_add_flg_h_l or eu_add_flg_h_l or asia_add_flg_h_l:
-            post_str = '@here 【diablo2.io diablo clone tracker(hardcore-ladder)】'
+            post_str = '@here 【diablo2.io diablo clone tracker(hardcore-ladder)】(連続の増加を検知)'
         else:
             post_str = '【diablo2.io diablo clone tracker(hardcore-ladder)】'
 

d99334f001e1e6721758babc1f0dfd231bb18433
swirhen <swirhen@gmail.com>
2023-01-25 14:22:34 +0900

    mysql local ok

--- tiarrakw.sh
+++ tiarrakw.sh
@@ -3,5 +3,5 @@ docker kill `cat /home/swirhen/tiarra/docker.id`
 #ps -ef | grep tiarra.conf | grep -v grep | awk '{print $2}' | xargs kill -9
 sleep 5
 cd /home/swirhen/tiarra
-docker run --name tiarra --add-host=myhost:host-gateway --network host -d --rm -v /home/swirhen/tiarra/:/tiarra/ -v /var/www/tm/:/var/www/tm/ perl-tiarra /bin/sh -c '/tiarra/tiarra --config=/tiarra/tiarra.conf' > docker.id
+docker run --name tiarra --network host -d --rm -v /home/swirhen/tiarra/:/tiarra/ -v /var/www/tm/:/var/www/tm/ perl-tiarra /bin/sh -c '/tiarra/tiarra --config=/tiarra/tiarra.conf' > docker.id
 #nohup /home/swirhen/sh/perlenv.sh /home/swirhen/tiarra/tiarra --config=/home/swirhen/tiarra/tiarra.conf >/dev/null 2>&1 &

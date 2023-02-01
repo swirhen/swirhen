775750590ca432733fd1082fd4450bf4cb9fa0cd
swirhen <swirhen@gmail.com>
2023-02-01 11:36:25 +0900

    ろぐをとるようにした あがりなおしたあとついったーこんそーるをぐるーぷもーどに

--- tiarrakw.sh
+++ tiarrakw.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-docker kill `cat /home/swirhen/tiarra/docker.id`
 #ps -ef | grep tiarra.conf | grep -v grep | awk '{print $2}' | xargs kill -9
+#sleep 10
+#nohup /home/swirhen/sh/perlenv.sh /home/swirhen/tiarra/tiarra --config=/home/swirhen/tiarra/tiarra.conf >/dev/null 2>&1 &
+docker kill `cat /home/swirhen/tiarra/docker.id`
 sleep 5
 cd /home/swirhen/tiarra
-docker run --name tiarra --network host -d --rm -v /home/swirhen/tiarra/:/tiarra/ -v /var/www/tm/:/var/www/tm/ perl-tiarra /bin/sh -c '/tiarra/tiarra --config=/tiarra/tiarra.conf' > docker.id
-#nohup /home/swirhen/sh/perlenv.sh /home/swirhen/tiarra/tiarra --config=/home/swirhen/tiarra/tiarra.conf >/dev/null 2>&1 &
+docker run --name tiarra --network host -d --rm -v /home/swirhen/tiarra/:/tiarra/ -v /var/www/tm/:/var/www/tm/ swirhen/perl4tiarra /bin/sh -c '/tiarra/tiarra --config=/tiarra/tiarra.conf > /tiarra/tiarra.log' > docker.id
+sleep 5
+/usr/bin/php /home/swirhen/tiasock/tiasock.php "#Console@t" "g"

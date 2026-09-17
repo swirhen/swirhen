7b69ff1983083504ffc19f80585ffcc66e639da9
swirhen <swirhen@gmail.com>
2026-09-17 23:29:56 +0900

    Remove checker/torrentsearch and untrack it (moved to separate repo)

--- checker/torrentsearch/web/vite.config.ts
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ base: process.env.VITE_BASE_PATH ?? '/', plugins: [react()], server: { host: '127.0.0.1', proxy: { '/api': 'http://127.0.0.1:8000' } } })
